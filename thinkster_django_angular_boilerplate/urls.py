632b97cc2ff45ae4f653fbccb98855bb95ef5b45
Savio Abuga <savioabuga@gmail.com>
2014-11-13 14:54:11 +0300

    Created a register.html template

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 from django.views.generic.base import TemplateView
+from authentication.views import UserCreateView
 
 urlpatterns = patterns(
     '',
-
+    url('^api/v1/users/$', UserCreateView.as_view(), name='user_create'),
+    url('^', TemplateView.as_view(template_name='static/index.html')),
     url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
