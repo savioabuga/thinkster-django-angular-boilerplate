ea745a5e272fa34e6110495c3888c5a00f9c4ce6
Savio Abuga <savioabuga@gmail.com>
2014-11-13 12:57:05 +0300

    Started the project
    Extended django User model
    Did the serializing with django rest framework

---
+++ authentication/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
